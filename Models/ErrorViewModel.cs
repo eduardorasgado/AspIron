57bbdc1d3c8fa8bb38ccda616e15ffae9bdbe8d4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-26 17:03:52 -0600

    Project started: ASP Core MVC.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspIron.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
