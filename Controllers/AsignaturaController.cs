cdcba2d36a25df80c95d95c8a67368b2955dd6f4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-28 23:12:18 -0600

    Asignatura: Index separated from MultiAsignatura.

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -7,13 +7,25 @@ namespace AspIron.Controllers
 {
     public class AsignaturaController : Controller
     {
+        public IActionResult Index()
+        {
+            //
+            var assig = new Asignatura
+            {
+                Nombre = "Programación Básica",
+                FechaDeLanzamiento = generateRandomUTC()
+            };
+            
+            return View(assig);
+        }
+        
         /// <summary>
         /// TO have a global view and management of all
         /// the asignaturas in the Academy
         /// GET
         /// </summary>
         /// <returns></returns>
-        public IActionResult Index()
+        public IActionResult MultiAsignatura()
         {
             var allAsignaturas = new List<Asignatura>
             {
@@ -38,7 +50,8 @@ namespace AspIron.Controllers
                     FechaDeLanzamiento = generateRandomUTC()
                 }
             };
-            return View(allAsignaturas);
+            // we can specify what view do we want to send the data to
+            return View("MultiAsignatura", allAsignaturas);
         }
 
         private DateTime generateRandomUTC()
@@ -46,7 +59,9 @@ namespace AspIron.Controllers
             Random gen = new Random();
             DateTime start = new DateTime(2018, 1, 1);
             int range = (DateTime.Today - start).Days;
-            return start.AddDays((gen.Next(range)));
+            
+            return start.AddDays((gen.Next(range)))
+                .AddHours(gen.Next(0,12));
         }
     }
 }
