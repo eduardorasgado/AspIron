4fc3058cf25a3464dac026f0f9c4c60215f2f475
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-29 02:09:59 -0600

    Alumno: GenerarAlumnosAlAzar method to create random Alumnos instances in controller

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using AspIron.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AspIron.Controllers
 {
@@ -17,7 +18,31 @@ namespace AspIron.Controllers
         public IActionResult MultiAlumno()
         {
             var listaAlumnos = new List<Alumno>();
+            listaAlumnos.AddRange(GenerarAlumnosAlAzar());
+            
             return View(listaAlumnos);
+        }
+        
+        // UTILITIES
+        private List<Alumno> GenerarAlumnosAlAzar()
+        {
+            string[] nombres = {"Eduardo", "Mario", "Francisco", "Manuel", "Fabian", "Mariana", "Victor"};
+            string[] apellido1 = {"Rasgado", "Guzman", "Olmedo", "Santiago", "Peña", "Jimenez", "Amampour", "Bartolo"};
+            string[] apellido2 = {"Ruiz", "Mendoza", "Cabrera", "Manrriquez", "Torres", "Santiago", "Alvarado"};
+            
+            // Utilizamos linq para crear combinatorias random de nombres
+            // linq son expresiones de consultas
+            // Language-Integrated Query
+            //
+            // aqui estamos creando un producto cartesiano con los arrays de strings arriba escritos
+            var listaAlumnos = from n in nombres
+                from a1 in apellido1
+                from a2 in apellido2
+                select new Alumno{Nombre = $"{n} {a1} {a2}"};
+            // retornamos para asignarlo a cada uno de los cursos
+            // retorna cierta cantidad y en un orden basado en el UniqueId
+            return listaAlumnos.OrderBy((alumno) => alumno.UniqueId).
+                Take(20).ToList();
         }
     }
 }
