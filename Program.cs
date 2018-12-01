80b4be503c762661f632f2d34ebcdee191a873b2
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-30 20:44:24 -0600

    In Program.Main: accessing the context and be sure database is ready before host start running.

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AspIron.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AspIron
@@ -17,7 +19,29 @@ namespace AspIron
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            // be sure database is working
+            var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                // accessing the context before host start running
+                var context = services.GetRequiredService<AcademyContext>();
+
+                // be sure about db is connected
+                context.Database.EnsureCreated();
+            }
+            catch (Exception ex)
+            {
+                // if error: log
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "[An error has ocurred creating the database.]");
+            }
+
+            // now we can be sure to run the web hoster
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
